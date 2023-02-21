e39d02718436b00cc1224bb4ba39c866ced24998
Your Name <you@example.com>
2023-02-21 20:10:26 +0300

    ddf

--- laba1-4.py
+++ laba1-4.py
@@ -1,4 +1,4 @@
-if a != 'красный' and a !='желтый' and a !='синий':
-   print('ошибка цвета')
-elif b != 'красный' and b !='желтый' and b !='синий':
-   print('ошибка цвета') 
+map_ = {frozenset(("красный", "синий")): "фиолетовый", frozenset(("красный", "желтый")): "оранжевый", frozenset(("синий", "желтый")): "зеленый", **{frozenset((color,)): color for color in ("красный", "синий", "желтый")}}
+from_, to = (input() for _ in range(2))
+result = map_.get(frozenset((from_, to)), "ошибка цвета")
+print(result)
